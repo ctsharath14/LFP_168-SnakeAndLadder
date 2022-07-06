f222ab1972539b4ee20be9fd2e18153160f44daa
ctshara14 <ctsharath14@gmail.com>
2022-07-06 16:09:59 +0530

    Adding: Roll a die to get a number between 1 to 6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,10 +1,14 @@
+//The Player rolls the die to get a number between 1 to 6.
+
 public class SnakeAndLadder {
 	
 	public static final int START_POSITION = 0;
-	
+
 
 	public static void main(String[] args) {
 		System.out.println("<--------Welcome to Snake And Ladder Game---------->");
 		System.out.println("Player start Position is: "+START_POSITION);
+		int diceNumber = (int) ((Math.random() * 6) + 1);
+		System.out.println("Dice Number : " +diceNumber);
 	}
 }
