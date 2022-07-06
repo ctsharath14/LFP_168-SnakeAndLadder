2fd824cbf8b7993a682297576efd32f3b4e26569
ctshara14 <ctsharath14@gmail.com>
2022-07-06 16:00:32 +0530

    Adding: Single Player Start at Position 0

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,12 +1,10 @@
-//Snake and Ladder game played with single player at start position 0
-
 public class SnakeAndLadder {
 	
-	public static int startPosition = 0;
+	public static final int START_POSITION = 0;
 	
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Snake And The Ladder Game");
-		System.out.println("Player Start Position is: "+startPosition);
+		System.out.println("<--------Welcome to Snake And Ladder Game---------->");
+		System.out.println("Player start Position is: "+START_POSITION);
 	}
 }
