7a0dbfd921c5276476ec802daeff34427c9419f7
ctshara14 <ctsharath14@gmail.com>
2022-07-11 11:10:03 +0530

    Adding: Repeat till reaches position 100

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,4 +1,4 @@
-//The Player then checks for a Option. They are No Play, Ladder or Snake.
+//Repeat till the Player reaches the winning position 100.
 
 public class SnakeAndLadder {
 	public static void main(String[] args) {
@@ -7,22 +7,45 @@ public class SnakeAndLadder {
 		int isSnake = 1;
 		int isLadder = 2;
 		int currentPosition = 0;
-		System.out.println("<--------Welcome to Snake And Ladder Game---------->");
+		System.out.println("<------------------Welcome to Snake And Ladder Game-------------------->");
 		System.out.println("Player start Position is: "+startPosition);
-		
-		int diceNumber = (int) ((Math.random() * 6) + 1);
-		System.out.println("Dice Number : " +diceNumber);
-		
-		int option = (int) ((Math.random() * 3) + 1); 
-		if (option == isSnake) {
-			currentPosition -= diceNumber;
-			System.out.println("Snake bites....");
-		} else if (option == isLadder) {
-			currentPosition += diceNumber;
-			
-		} else if (option == noPlay) {
-			System.out.println("NO Play");
-		}
+	        do {
+		System.out.println("<---------------------------------------------------------------------------->");
+            	int diceNumber = (int) ((Math.random() * 6) + 1);
+            	System.out.println("Dice number after rolling is : " +diceNumber);
+            	int option = (int) (Math.random() * 3);
+            	switch(option) {
+                case 0:
+                    System.out.println("NO PLAY!! Players stays at the same position");
+                    break;
+                case 1:  
+                    if((currentPosition+diceNumber) <= 100){
+                        currentPosition += diceNumber; 
+                    	if(currentPosition == 100){
+                            System.out.println("Player reaches winning position 100");
+                            System.exit(0);
+                            }
+			else{
+                            System.out.println("Player Moved to position: "+ currentPosition);
+                        }
+                    }
+			else{
+                        System.out.println("Die output and current position sum is larger than 100 chance skipped");
+                    }
+                    break;
+                case 2:
+                    if((currentPosition - diceNumber) >= 0){
+                        currentPosition -= diceNumber;
+                        System.out.println("Snake bites!! Player Move Back to position : "+currentPosition);
+                    	}
+		    else{
+                        currentPosition = 0;
+                        System.out.println("Snake bites!! Player Moved Again to starting position");
+                    }
+                    break;
+            }
+        }while(currentPosition <= 100);
 
-	}
-}
+        }
+}	
+	
