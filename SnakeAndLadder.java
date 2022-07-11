66de14abd6b1317e1cdf6bb52c40729af019e4b6
ctshara14 <ctsharath14@gmail.com>
2022-07-11 11:07:30 +0530

    Adding: Player checks for an option

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,11 +1,28 @@
-//The Player rolls the die to get a number between 1 to 6.
+//The Player then checks for a Option. They are No Play, Ladder or Snake.
 
 public class SnakeAndLadder {
 	public static void main(String[] args) {
 		int startPosition = 0;
+		int noPlay = 0;
+		int isSnake = 1;
+		int isLadder = 2;
+		int currentPosition = 0;
 		System.out.println("<--------Welcome to Snake And Ladder Game---------->");
 		System.out.println("Player start Position is: "+startPosition);
+		
 		int diceNumber = (int) ((Math.random() * 6) + 1);
 		System.out.println("Dice Number : " +diceNumber);
+		
+		int option = (int) ((Math.random() * 3) + 1); 
+		if (option == isSnake) {
+			currentPosition -= diceNumber;
+			System.out.println("Snake bites....");
+		} else if (option == isLadder) {
+			currentPosition += diceNumber;
+			
+		} else if (option == noPlay) {
+			System.out.println("NO Play");
+		}
+
 	}
 }
